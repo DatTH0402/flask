52ff76e25ea7a331072ea5853dabe5c5d0ea69a1
DatTH0402 <tranhuudat.0402.developer@gmail.com>
2023-06-12 09:09:49 +0700

    form, using redirect and session

--- hello.py
+++ hello.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, session, url_for, redirect
 from datetime import datetime
 from flask_bootstrap import Bootstrap
 from flask_moment import Moment
@@ -20,9 +20,9 @@ def index():
     name = None
     form = NameForm()
     if form.validate_on_submit():
-        name = form.name.data
-        form.name.data = ''
-    return render_template("index.html", form=form, name=name, current_time=datetime.utcnow())
+        session['name'] = form.name.data
+        return redirect(url_for('index'))
+    return render_template("index.html", form=form, name=session.get('name'), current_time=datetime.utcnow())
 
 @app.route('/user/<name>')
 def user(name):
